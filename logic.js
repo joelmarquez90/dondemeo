05babbda8b5d056cd191fdeccd4aa9248d2a43cd
Joel Marquez <90joelmarquez@gmail.com>
2024-08-31 21:04:35 -0300

    Improvements

--- logic.js
+++ logic.js
@@ -5,41 +5,56 @@ function game() {
         gameStarted: false,
         gameEnded: false,
 
+        init() {
+            this.startGame();
+        },
+
         startGame() {
-            const numUrinals = Math.floor(Math.random() * 6) + 5; // Between 5 and 10 urinals
-            this.urinals = Array(numUrinals).fill(false);
+            const numUrinals = Math.floor(Math.random() * 6) + 5; // Entre 5 y 10 mingitorios
+            this.urinals = Array(numUrinals).fill('free');
             
-            // Occupy some urinals randomly
+            // Ocupar algunos mingitorios aleatoriamente
             for (let i = 0; i < this.urinals.length; i++) {
-                if (Math.random() < 0.3) { // 30% chance of being occupied
-                    this.urinals[i] = true;
+                if (Math.random() < 0.3) { // 30% de probabilidad de que esté ocupado
+                    this.urinals[i] = 'occupied';
                 }
             }
             
-            this.message = '';
+            this.message = 'Elegí un mingitorio libre, mejor lejos de otros!';
             this.gameStarted = true;
             this.gameEnded = false;
         },
 
         selectUrinal(index) {
-            if (this.gameEnded) return;
+            if (this.gameEnded || this.urinals[index] !== 'free') return;
 
-            if (this.urinals[index]) {
-                this.message = 'Apa..';
-                return;
-            }
-            
             let adjacentOccupied = false;
-            if (index > 0 && this.urinals[index - 1]) adjacentOccupied = true;
-            if (index < this.urinals.length - 1 && this.urinals[index + 1]) adjacentOccupied = true;
+            if (index > 0 && this.urinals[index - 1] === 'occupied') adjacentOccupied = true;
+            if (index < this.urinals.length - 1 && this.urinals[index + 1] === 'occupied') adjacentOccupied = true;
+            
+            this.urinals[index] = 'selected';
             
             if (adjacentOccupied) {
                 this.message = 'Querés pispear?';
             } else {
-                this.message = 'Bien chad!';
+                let optimalChoice = true;
+                for (let i = 0; i < this.urinals.length; i++) {
+                    if (this.urinals[i] === 'free' && 
+                        (i === 0 || this.urinals[i-1] !== 'occupied') && 
+                        (i === this.urinals.length - 1 || this.urinals[i+1] !== 'occupied')) {
+                        if (i !== index) {
+                            optimalChoice = false;
+                            break;
+                        }
+                    }
+                }
+                if (optimalChoice) {
+                    this.message = 'Elegiste el mejor lugar';
+                } else {
+                    this.message = 'Porías haber elegido mejor..';
+                }
             }
 
-            this.urinals[index] = true;
             this.gameEnded = true;
         }
     }
